86b1ff171d78976953706370e0ef70d4a837270b
messner1 <messner1@cdh>
2025-02-27 17:01:59 -0500

    alignment fixes

--- scripts/apply_model.py
+++ scripts/apply_model.py
@@ -2,6 +2,9 @@ import argparse
 from transformers import AutoTokenizer, LlamaForCausalLM
 import torch
 import json
+from Levenshtein import distance
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -10,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument("--max_tokens", type=int, default=50, help="max new tokens to generate")
     parser.add_argument("--model", default="meta-llama/Meta-Llama-3-8B")
     parser.add_argument("--output", dest="output", help="Output files")
-    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--device", default="cuda")
     parser.add_argument("--random_state", type=int, default=20)
     args, rest = parser.parse_known_args()
@@ -20,17 +23,24 @@ if __name__ == "__main__":
 
     with open(args.input, "rt") as p_in:
         poem = p_in.read()
+        poem = poem.replace("'", "’").replace('"', '“').replace('"', '”')
 
-    tokenized = tokenizer(poem, max_length=args.window, stride=48, truncation=True, padding=True, return_overflowing_tokens=True, return_tensors="pt").input_ids
+    tokenized = tokenizer(poem, max_length=args.window, stride=49, truncation=True, padding=True, return_overflowing_tokens=True, return_tensors="pt", add_special_tokens=False).input_ids
+    targets = tokenized[args.window:]
+    targets = torch.cat((targets, torch.full((1,50), tokenizer.pad_token_id)),axis=0)
+
     model = LlamaForCausalLM.from_pretrained(args.model, device_map="auto")
     batched = torch.split(tokenized, args.batch_size)
+    batched_targets = torch.split(targets, args.batch_size)
     with open(args.output, "wt") as jout:
-        for batch in batched:
-            stems = tokenizer.batch_decode(batch, clean_up_tokenization_spaces=False, skip_special_tokens=True) 
+        for batch, b_t in zip(batched, batched_targets):
+            stems = tokenizer.batch_decode(batch, clean_up_tokenization_spaces=False, skip_special_tokens=True)
+            targets = tokenizer.batch_decode(b_t, clean_up_tokenization_spaces=False, skip_special_tokens=True)
             res = model.generate(batch.to(args.device), max_new_tokens=args.max_tokens, min_new_tokens=args.max_tokens, return_dict_in_generate=True, do_sample=False, pad_token_id=tokenizer.eos_token_id)
             generated = tokenizer.batch_decode(res.sequences[:, batch.shape[1]:], clean_up_tokenization_spaces=False, skip_special_tokens=True)
-            for stem, gen in zip(stems, generated):
-                jout.write(json.dumps({"stem": stem, "cont": gen})+"\n")
+            for stem, gen, target in zip(stems, generated, targets):
+                dis = distance(gen, target)
+                jout.write(json.dumps({"stem": stem, "cont": gen, "target":target, "LD":dis})+"\n")
 
 
 
